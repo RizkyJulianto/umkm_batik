04ef2df1f9fb00bf908fac334d9b25dedbfb7959
Juliantomaga <juliantorizky01@gmail.com>
2025-05-25 15:22:29 +0700

    make beranda section

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/tailwindcss/input.css">
+    <link rel="stylesheet" href="assets/css/tailwindcss/output.css">
 
     <!-- Flowbite -->
     <link rel="stylesheet" href="libs/flowbite/flowbite.min.css">
@@ -20,7 +20,7 @@
                     <span class="self-center text-2xl font-bold whitespace-nowrap dark:text-black/80">Batik Indonesia</span>
                 </a>
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  ">
                         <li>
                             <a href="#" class="block py-2 px-3 text-black/70 dark:text-black/70 " aria-current="page">Beranda</a>
                         </li>
@@ -39,6 +39,32 @@
         </nav>
 
     </header>
+
+    <!-- Header Section End -->
+
+    <!-- Beranda Section -->
+    <div class="beranda bg-cover h-screen">
+        <!-- Backround -->
+         <img src="assets/images/beranda-bg.png" alt="" class="absolute -z-10 top-0 w-full">
+        <div class="beranda-section-box-text text-center text-black mt-12 flex flex-col items-center gap-y-2">
+            <div class="beranda-section-text-title">
+                <h2 class="font-bold text-black/80 lg:text-[40px] md:text-4xl text-[35px] ">
+                    Batik <span class="text-primary">Indonesia</span> Membentuk Pola <br>
+                    Melestarikan <span class="text-primary">Budaya</span>
+                </h2>
+            </div>
+            <div class="beranda-section-text-subtitle mt-4">
+                <p class="lg:text-lg text-base font-light text-center max-w-[720px]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto provident perspiciatis ipsa aut repellat quis cumque voluptatibus veniam inventore.
+                </p>
+            </div>
+            <div class="beranda-section-cta mt-6">
+                <a href="" class="bg-brown text-white py-3 px-6 rounded-full font-semibold transition-all duration-300 hover:bg-darkbrown">Lihat Menu</a>
+          i/div>
+        </div>
+
+    </div>
+    <!-- Beranda Section End -->
 
     <script src="libs/flowbite/flowbite.min.js"></script>
 </body>
