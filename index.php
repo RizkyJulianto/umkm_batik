cc4213e3972ddbda0592608dcd87c5d7a6fee145
Juliantomaga <juliantorizky01@gmail.com>
2025-05-25 14:46:23 +0700

    make navbar

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/tailwindcss/input.css">
+
+    <!-- Flowbite -->
+    <link rel="stylesheet" href="libs/flowbite/flowbite.min.css">
+    <title>Batik Indonesia</title>
+</head>
+
+<body>
+    <!-- Header Section -->
+    <header class="header">
+        <nav class="bg-transparant py-5 px-8">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span class="self-center text-2xl font-bold whitespace-nowrap dark:text-black/80">Batik Indonesia</span>
+                </a>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
+                        <li>
+                            <a href="#" class="block py-2 px-3 text-black/70 dark:text-black/70 " aria-current="page">Beranda</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 px-3 text-black/70 rounded-sm">Tentang</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 px-3 text-black/70 rounded-sm">Produk</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 px-3 text-black/70 rounded-sm">Testimoni</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
+    <script src="libs/flowbite/flowbite.min.js"></script>
+</body>
+
+</html>
