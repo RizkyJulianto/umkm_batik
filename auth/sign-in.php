6d2b6a71dca9976e395393850e816d75e9b26b86
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2025-05-25 14:39:19 +0700

    Add configurations Font

--- auth/sign-in.php
+++ auth/sign-in.php
@@ -5,9 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/tailwindcss/output.css">
 </head>
 
 <body>
+    <section class="sign-container">
+        <main class="sign-row">
+            <div class="left-sign">
+                <h1>Batik Indonesia</h1>
+            </div>
+            <div class="right-sign">
+                <form action="">
+                    <div class="f-group">
+                        <label for="" class="l-group">Email</label>
+                        <input type="text" name="email" placeholder="Masukan email anda" id="email">
+                    </div>
+                    <div class="f-group">
+                        <label for="" class="l-group">Password</label>
+                        <input type="text" name="email" placeholder="Masukan email anda" id="email">
+                    </div>
+                </form>
+            </div>
+        </main>
+    </section>
 
 </body>
 
